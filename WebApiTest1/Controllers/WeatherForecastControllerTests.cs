164709b7ae874c7cfba6e3bfde4268b74fd9f8f2
waqar <wakku9780@gmail.com>
2024-05-23 12:39:26 +0530

    Add project files.

---
+++ WebApiTest1/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using WebApplication3.Controllers;
+
+namespace WebApplication3.Tests
+{
+    [TestClass]
+    public class WeatherForecastControllerTests
+    {
+        private Mock<ILogger<WeatherForecastController>> _loggerMock;
+        private WeatherForecastController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            _controller = new WeatherForecastController(_loggerMock.Object);
+        }
+
+        [TestMethod]
+        public void Get_ReturnsWeatherForecasts()
+        {
+            // Act
+            var result = _controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(5, result.Count());
+        }
+
+        [TestMethod]
+        public void Get_ReturnsDifferentWeatherForecasts()
+        {
+            // Act
+            var result = _controller.Get().ToList();
+
+            // Assert
+            for (int i = 0; i < result.Count - 1; i++)
+            {
+                Assert.AreNotEqual(result[i].Summary, result[i + 1].Summary);
+                Assert.AreNotEqual(result[i].Date, result[i + 1].Date);
+            }
+        }
+    }
+}
---
+++ WebApiTest1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebApiTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
